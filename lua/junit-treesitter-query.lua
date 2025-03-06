2ea611bd3ce649365d5bae3f6a4293b40bdd2011
Christian Howell <chowell@firstorion.com>
2025-03-05 22:51:07 -0600

    more query fiddling

--- lua/junit-treesitter-query.lua
+++ lua/junit-treesitter-query.lua
@@ -5,17 +5,17 @@ TreesitterQuery.value = [[
 ;; Capture Kotlin test functions with @Test and @ParameterizedTest annotations
 (
   (function_declaration
-    name: (simple_identifier) @test.name
-    body: (function_body) @function.body
     (modifiers
       (annotation
        (constructor_invocation
         (user_type) @AnnotationFunctionName
+        (value_arguments (string_literal) @test.name)?
        )
       )
     )
+    name: (simple_identifier) @test.name
   ) @test.definition
-  (#match? @AnnotationFunctionName "Test$|ParameterizedTest") ;; Match both @Test and @ParameterizedTest
+  (function_body) @function.body (#match? @AnnotationFunctionName "ParameterizedTest")
 )
 
 ]]
