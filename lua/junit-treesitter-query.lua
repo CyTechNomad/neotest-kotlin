aff3b41f1c8344cc41a2fdf104424247c9223f0f
Christian Howell <chowell@firstorion.com>
2025-03-06 11:41:39 -0600

    hopefuly finds the tests now

--- lua/junit-treesitter-query.lua
+++ lua/junit-treesitter-query.lua
@@ -6,9 +6,9 @@ TreesitterQuery.value = [[
 (function_declaration
     (modifiers
       (annotation
-        (constructor_invocation
           (user_type (type_identifier) @annotation_name (#eq? @annotation_name "Test"))))
-    (simple_identifier @test.name)
+    (simple_identifier) @test.name
 )@test.definition
+
 ]]
 return TreesitterQuery
