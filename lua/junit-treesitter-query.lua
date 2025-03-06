fe21b1c976513ddc1fb85cf87403b0b7e36d9747
Christian Howell <chowell@firstorion.com>
2025-03-06 09:42:07 -0600

    trying out eq? instead of match?

--- lua/junit-treesitter-query.lua
+++ lua/junit-treesitter-query.lua
@@ -3,14 +3,12 @@ local TreesitterQuery = {}
 TreesitterQuery.value = [[
 
 ;; Capture Kotlin test functions with @Test annotations
-(
-  (function_declaration
+(function_declaration
     (modifiers
       (annotation
         (constructor_invocation
-          (user_type (type_identifier) @annotation.name))))
-    (simple_identifier @test.name))
-) @test.definition (#match? @annotation.name "Test")
-
+          (user_type (type_identifier) @annotation_name (#eq? @annotation_name "Test"))))
+    (simple_identifier @test.name)
+)@test.definition
 ]]
 return TreesitterQuery
